5070e2cd78eb5bea0207cf8ebaaccdc040520104
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-13 20:23:26 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/*main-Serves as the entry point for the program*/
+/*Return:By default returns zero if no errors otherwise returns non-zero value*/
+int main(void)
+{
+puts("\"Progarmming is like building a multilingual puzzle\n");
+return (0);
+}
