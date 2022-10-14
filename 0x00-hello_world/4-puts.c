bd5057a53af1cf5bc20f15121a9c091b509e4639
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-13 20:25:51 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /*Return:By default returns zero if no errors otherwise returns non-zero value*/
 int main(void)
 {
-puts("\"Progarmming is like building a multilingual puzzle\n");
+puts("\"Progarmming is like building a multilingual puzzle");
 return (0);
 }
