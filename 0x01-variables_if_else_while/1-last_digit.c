8fee8b32f44c59c204bfe0efc8f743990b9c40e0
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-18 17:40:16 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@ int main(void)
 {
 
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -23,10 +24,10 @@ int main(void)
 	{
 		printf("and is 0");
 	}
-	if (x < 6 && x !=0)
+	if (x < 6 && x != 0)
 	{
 		printf("and is less than 6 and not 0");
 	}
 	printf("\n");
-	return (0);	
+	return (0);
 }
