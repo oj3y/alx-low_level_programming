30ce55b8f6b0928e65fd2b98708807133bbab5e4
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-23 02:46:40 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -> assign random number into n as it is executed and print
+ * the last digit of the number stored in n
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
