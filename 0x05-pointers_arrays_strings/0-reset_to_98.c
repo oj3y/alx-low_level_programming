b5be285813c4b5640de58f3dfe4cbcf6acf53874
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-24 14:17:33 -0700

    no be so

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the pointer and updates the valu
+ * 
+ * e@n: int to check
+ *
+ * Owned By Bwave/Bright
+ *
+ * Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
