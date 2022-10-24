91e03da67f4dcd4c8adc429f9b5f5ae6e14b5329
Ojeikere Ohikhateeme <oziegbeoje@gmail.com>
2022-10-24 16:58:25 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print alphabets - prints lower case alphabets  followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
